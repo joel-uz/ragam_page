fccd6e22abaff061bbe6147482805ab5f2884498
joel john <j2joelgeorgejohn@gmail.com>
2023-01-07 13:48:44 +0530

    'second'

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import styles from '../styles/Home.module.css'
 function Home(){
   return <div className={styles.main_layout}>
     <div className={styles.Body_page}>
-      <h1>Hello</h1>
+      <h1>Hello world</h1>
     </div>
   </div>
 }
