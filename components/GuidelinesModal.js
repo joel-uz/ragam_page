fbc1678ae86f8c570b3715f369ddd715cb9bfa50
Nazim Jabir <nazimjabir@gmail.com>
2023-02-01 19:22:46 +0530

    small ui bug fixed in reg details modal and added links except faq

--- components/GuidelinesModal.js
+++ components/GuidelinesModal.js
@@ -5,13 +5,10 @@ import styles from  "../styles/eachevent.module.css"
 const GuidelinesModal = ({guidelinesModalOpen,closeGuidelinesModal}) => {
     const data  =   {
         guidelines:[
-            'Tourner dans le vide vide Tourne dans le vie',
-            'Tourner dans le vide vide Tourne dans le vie',
-            'Tourner dans le vide vide Tourne dans le vie',
-            'Tourner dans le vide vide Tourne dans le vie',
-            'Tourner dans le vide vide Tourne dans le vie',
-            'Tourner dans le vide vide Tourne dans le vie',
-            'Tourner dans le vide vide Tourne dans le vie',
+            'You should be cautious when entering your information in the registration portal as it will appear on your certificates issued.',
+            'Before proceeding to fee payment, all participants must completely fill out the registration details. It is not possible to make any changes once you have entered your information.',
+            'Certificates will be issued to all workshop participants upon completion of the workshop.',
+            'If you have any queries, please feel free to contact one of our workshop coordinators.',
         ]
     }
   return (
@@ -22,6 +19,7 @@ const GuidelinesModal = ({guidelinesModalOpen,closeGuidelinesModal}) => {
           </Button>
         ]}
     >
+        <h3>Before registering for this workshop, please read the following instructions:</h3>
         <ol className={styles.modalPadding}>
             {
                 data.guidelines.map(x=>( <li    className={styles.listItemPadding}>{x}</li> ))
